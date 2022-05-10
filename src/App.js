1f529584f4ea9f21dc6a4f63610b9e118a4cd1f3
Akanksha <b.akanksha6@gmail.com>
2022-05-10 09:42:03 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <p>Initial setup</p>
     </div>
   );
